02820e74549fb959249a58665d07885b397fb9d1
bhargav <bhargavreddypingle@gmail.com>
2021-07-25 12:00:00 -0500

    main file updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package src;
+
+import src.vector.Vector;
+import src.matrix.Matrix;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Vector v1 = new Vector(new double[]{1, 2, 3});
+        Vector v2 = new Vector(new double[]{4, 5, 6});
+        System.out.println("Vector Addition: " + v1.add(v2));
+        System.out.println("Dot Product: " + v1.dot(v2));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double[][] data1 = {
+                {1, 2},
+                {3, 4}
+        };
+        double[][] data2 = {
+                {5, 6},
+                {7, 8}
+        };
+
+        Matrix m1 = new Matrix(data1);
+        Matrix m2 = new Matrix(data2);
+        System.out.println("Matrix Addition:");
+        m1.add(m2).print();
+
+        System.out.println("Matrix Product:");
+        m1.multiply(m2).print();
+
+        System.out.println("Determinant: " + m1.determinant());
     }
 }
